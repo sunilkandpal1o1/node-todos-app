a5093f2e40ada656afd533dc0e7c7e4f8f0bc492
Sunil Kandpal <sskandpal.uk@gmail.com>
2020-03-25 16:27:13 +0530

    Add test for GET /todos/id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
 
+const {ObjectID} = require('mongodb');
+
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
 
 let todos = [{
+	_id: new ObjectID(),
 	text: "first thing to do"
 },{
+	_id: new ObjectID(),
 	text: "second thing to do"
 }];
 
@@ -21,40 +25,40 @@ describe('POST /todos route', () => {
 		let text = "todo testing";
 
 		request(app)
-		.post('/todos')
-		.send({text})
-		.expect(200)
-		.expect((res) => {
-			expect(res.body.text).toBe(text);
-		})
-		.end((err, res) => {
-			if(err) {
-				return done(err);
-			}
+			.post('/todos')
+			.send({text})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.text).toBe(text);
+			})
+			.end((err, res) => {
+				if(err) {
+					return done(err);
+				}
 
-			Todo.find({text}).then((todos) => {
-				expect(todos.length).toBe(1);
-				expect(todos[0].text).toBe(text);
-				done();
-			}, (err) => done(err));
-		});
+				Todo.find({text}).then((todos) => {
+					expect(todos.length).toBe(1);
+					expect(todos[0].text).toBe(text);
+					done();
+				}, (err) => done(err));
+			});
 	});
 
 	it('should not create a new todo(invalid text body)', (done) => {
 		request(app)
-		.post('/todos')
-		.send({})
-		.expect(400)
-		.end((err, res) => {
-			if(err) {
-				return done(err);
-			}
+			.post('/todos')
+			.send({})
+			.expect(400)
+			.end((err, res) => {
+				if(err) {
+					return done(err);
+				}
 
-			Todo.find().then((todos) => {
-				expect(todos.length).toBe(2);
-				done();
-			}).catch((err) => done(err));
-		});
+				Todo.find().then((todos) => {
+					expect(todos.length).toBe(2);
+					done();
+				}).catch((err) => done(err));
+			});
 	});
 });
 
@@ -69,3 +73,32 @@ describe('GET /todos route', () => {
 			.end(done);
 	});
 });
+
+describe('GET /todos/:id route', () => {
+	it('should return todo doc', (done) => {
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(todos[0].text);
+			})
+			.end(done);
+	});
+
+	it('should return 404 if todo not found', (done) => {
+		id = new ObjectID();
+		request(app)
+			.get(`/todos/${id.toHexString()}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 for non ObjectID', (done) => {
+		id = 123;
+		request(app)
+			.get(`/todos/${id}`)
+			.expect(404)
+			.end(done);
+	});
+});
+
