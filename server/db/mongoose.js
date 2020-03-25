0f1914260dbdc98c127044ef148ceb7ab5d7ec2d
Sunil Kandpal <sskandpal.uk@gmail.com>
2020-03-26 01:24:06 +0530

    Fixed problems with the mogodb uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+// const MONGODB_URI = 'mongodb://admin:h3fay5XTYmaR8PS@cluster0-shard-00-00-ofbrv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
