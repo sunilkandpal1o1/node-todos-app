b655538ffc509a0ff68df36307c78c903a9d2650
Sunil Kandpal <sskandpal.uk@gmail.com>
2020-03-26 19:00:22 +0530

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -52,7 +52,28 @@ app.get('/todos/:id', (req, res) => {
 		res.status(400).send(err);
 	});
 
-})
+});
+
+app.delete('/todos/:id', (req, res) => {
+	let id = req.params.id;
+
+	// res.send(req.params.id);
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo) {
+			res.status(404).send();
+		}
+
+		res.send(todo);
+	}, (err) => {
+		res.status(400).send(err);
+	});
+
+});
 
 app.listen(port, () => {
 	console.log(`Server started at ${port}`);
